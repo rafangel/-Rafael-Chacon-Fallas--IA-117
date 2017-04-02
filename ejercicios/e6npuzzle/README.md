640110552ce493f1e13a70a3ab985a15d1b4af6a
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-04-02 14:56:06 -0600

    V 1.0 cambios para legibilidad del codigo y su mejor comprension, asi como resultados obtenidos

--- ejercicios/e6npuzzle/README.md
+++ ejercicios/e6npuzzle/README.md
@@ -1,4 +1,7 @@
 # -Rafael-Chacon-Fallas--IA-117
 # N Puzzle
 ##Como correr el program
-   * ejecutar el archivo .java
+   * utilizar el comando cd hasta la carpeta del archivo .cpp
+   * ejecutar el comando gcc e6npuzzle.cpp
+   * ejecutar el comando ./a.out
+   * dgitar el input del programa
--- ejercicios/e6npuzzle/e6npuzzle.cpp
+++ ejercicios/e6npuzzle/e6npuzzle.cpp
@@ -16,9 +16,9 @@ int main(){
 	int Y=N;
     
     //this code uses map, vector, pair and queue data structures
-	map<vector<int>,pair<int,int> >m;
-	map<vector<int>,pair<vector<int>,string> >prev;
-	queue<vector<int> >q; //FIFO
+	map<vector<int>,pair<int,int>>m;
+	map<vector<int>,pair<vector<int>,string>>prev;
+	queue<vector<int>>q; //FIFO
 	vector<int>v;
     vector<int>next;
     
@@ -36,47 +36,55 @@ int main(){
         
 		int coor=m[v].first; // first value of pair
         
-        int x=coor%N;
+        int x=coor%N; //coordenates of space or 0
         int y=coor/N;
         
 		int depth=m[v].second; //second value of pair
 		next=v;
 		if(0<x){
-			swap(v[coor],v[coor-1]); //exchange the two variables values
-            if(m.find(v)==m.end()){
-                m[v]=make_pair(coor-1,depth+1),q.push(v),prev[v]=make_pair(next,"RIGHT");
+			swap(v[coor],v[coor-1]); //move O to left
+            if(m.find(v)==m.end()){ //use end() function as a heuristic, sorting the vector to a "higher" state
+                m[v]=make_pair(coor-1,depth+1);
+                q.push(v);
+                prev[v]=make_pair(next,"RIGHT");
             }
-			swap(v[coor],v[coor-1]);
+			swap(v[coor],v[coor-1]);//restore v
 		}
 		if(x<N-1){
-			swap(v[coor],v[coor+1]);
+			swap(v[coor],v[coor+1]); //move 0 to right
             if(m.find(v)==m.end()){
-                m[v]=make_pair(coor+1,depth+1),q.push(v),prev[v]=make_pair(next,"LEFT");
+                m[v]=make_pair(coor+1,depth+1);
+                q.push(v);
+                prev[v]=make_pair(next,"LEFT");
             }
-			swap(v[coor],v[coor+1]);
+			swap(v[coor],v[coor+1]);//restore v
 		}
 		if(0<y){
-			swap(v[coor],v[coor-N]);
+			swap(v[coor],v[coor-N]); //move 0 up
             if(m.find(v)==m.end()){
-                m[v]=make_pair(coor-N,depth+1),q.push(v),prev[v]=make_pair(next,"DOWN");
+                m[v]=make_pair(coor-N,depth+1);
+                q.push(v);
+                prev[v]=make_pair(next,"DOWN");
             }
-			swap(v[coor],v[coor-N]);
+			swap(v[coor],v[coor-N]);//restore v
 		}
 		if(y<Y-1){
-			swap(v[coor],v[coor+N]);
+			swap(v[coor],v[coor+N]); // move 0 down
             if(m.find(v)==m.end()){
-                m[v]=make_pair(coor+N,depth+1),q.push(v),prev[v]=make_pair(next,"UP");
+                m[v]=make_pair(coor+N,depth+1);
+                q.push(v);
+                prev[v]=make_pair(next,"UP");
             }
-			swap(v[coor],v[coor+N]);
+			swap(v[coor],v[coor+N]);//restore v
 		}
-        if(m.find(problem)!=m.end()){
+        if(m.find(problem)!=m.end()){ //finalization state (input state)
             break;
         }
 	}
 
-	printf("%d\n",m[problem].second);
+	printf("%d\n",m[problem].second); //total depth
 	while(prev[problem].first!=problem){
-		puts(prev[problem].second.c_str());
-		problem=prev[problem].first;
+		printf("%s\n",prev[problem].second.c_str());
+		problem=prev[problem].first; //advance problem to next step (vector) in prev
 	}
 }
