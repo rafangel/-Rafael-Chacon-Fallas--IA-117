ff82ee5d8387ed8464952fa88d72f5e12f465fc2
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-04-19 14:55:43 -0600

    primera prueba con threads

--- proyectos/Proyecto1/proyecto1.py
+++ proyectos/Proyecto1/proyecto1.py
@@ -1,16 +1,45 @@
 ##Proyecto 1 IA
 
 from tkinter import *
+import _thread as thread
+import time
+
 
 root = Tk()
 root.title("Proyecto 1 IA")
 root.geometry("900x640")#size of the root window
 root.resizable(0, 0) #Don't allow resizing in the x or y direction
 
+
+
+
+
+# Define a function for the thread
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print (threadName + " " + time.ctime(time.time()) )
+
+# Create two threads as follows
+try:
+   thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+   thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+   print("Error: unable to start thread")
+
+
+
+#********** File read ***************
+
 file = open("board.txt", "r")
 
 board = file.read()
 print(board)
+
+#************************************
+
 
 
 def send():
