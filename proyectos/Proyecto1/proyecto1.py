7292939964cafdde03c0184305ee0e17a73b43c3
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-04-19 14:43:16 -0600

    configuracion inicial de la ventana principal y lectura del mapa desde archivo txt

--- proyectos/Proyecto1/proyecto1.py
+++ proyectos/Proyecto1/proyecto1.py
@@ -1,6 +1,25 @@
 ##Proyecto 1 IA
 
-import tkinter
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+from tkinter import *
+
+root = Tk()
+root.title("Proyecto 1 IA")
+root.geometry("900x640")#size of the root window
+root.resizable(0, 0) #Don't allow resizing in the x or y direction
+
+file = open("board.txt", "r")
+
+board = file.read()
+print(board)
+
+
+def send():
+   messagebox.showinfo( "Hello Python", "Sending commmand")
+
+B = Button(root, text ="Send", command = send)
+B.place(x=500,y=500)
+
+E1 = Entry(root, bd =5)
+E1.place(x=300,y=500)
+
+root.mainloop()
