efb75b0cad955325156d6b4be3a838b38cdabaa7
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-04-25 23:12:37 -0600

    condiciones pararecibir instrucciones y avance en interfaz

--- proyectos/Proyecto1/proyecto1.py
+++ proyectos/Proyecto1/proyecto1.py
@@ -5,30 +5,47 @@ import _thread as thread
 import time
 
 global mapa
+global plainBoard
 global delay
+global mostrar
+global ruta
 
 root = Tk()
 root.title("Proyecto 1 IA")
 root.geometry("900x640")#size of the root window
 root.resizable(0, 0) #Don't allow resizing in the x or y direction
+root.configure(background='black')
 
+delay = 0
+global test
+
+
+def rePaint():
+   global test
+   test.destroy()
+   
+   test = Text(root, bg = "black",fg = "green")
+   test.insert(INSERT,plainBoard)
+   test.place(x=150,y=100)
 
 
 
 
 # Define a function for the thread
-def print_time( threadName):
+def threadFunction( threadName):
    global delay
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print (threadName + " " + time.ctime(time.time()) )
+   global mapa
+   while True:
+      if(delay == 0):
+         time.sleep(0.5)
+      else:
+         time.sleep(delay)
+         rePaint()
+         print (threadName + " " + time.ctime(time.time()) )
 
 # Create two threads as follows
 try:
-   delay = 4
-   thread.start_new_thread( print_time, ("Thread-1", ) )
+   thread.start_new_thread( threadFunction, ("Thread-1", ) )
    #thread.start_new_thread( print_time, ("Thread-2", ) )
 except:
    print("Error: unable to start thread")
@@ -39,8 +56,8 @@ except:
 
 file = open("board.txt", "r")
 
-board = file.read()
-mapa = board.split("\n")
+plainBoard = file.read()
+mapa = plainBoard.split("\n")
 print(mapa)
 
 #************************************
@@ -48,10 +65,63 @@ print(mapa)
 
 
 def send():
+   global delay
+   global mostrar
+   global ruta
    action = E1.get()
-   messagebox.showinfo( "Hello Python", action)
+   words = action.split(" ")
+   if(words[0] == "pasear" and len(words) == 1):
+      print("paseando")
+   elif(words[0] == "buscar" and len(words) == 1):
+      print("buscando")
+   elif(words[0] == "mostrar" and len(words) == 2):
+      if(words[1] == "on"):
+         print("mostrando on")
+         mostrar = True
+      if(words[1] == "off"):
+         print("mostrando off")
+         mostrar = False
+   elif(words[0] == "animar" and len(words) == 2):
+      try:
+         a = int(words[1])
+         delay = a/1000
+         print("animando: " + str(a))
+      except ValueError:
+         print("Error comando incorrecto")
+   elif(words[0] == "ruta" and len(words) == 2):
+      if(words[1] == "on"):
+         print("ruteando on")
+         ruta = True
+      if(words[1] == "off"):
+         print("ruteando off")
+         ruta = True
+   elif(words[0] == "clientes" and len(words) == 2):
+      try:
+         a = int(words[1])
+         print("colocando clientes: " + str(a))
+      except ValueError:
+         print("Error comando incorrecto")
+   elif(words[0] == "cliente" and len(words) == 3):
+      try:
+         a = int(words[1])
+         b = int(words[2])
+         print("colocando cliente: " + str(a) + " hacia: "+str(b))
+      except ValueError:
+         print("Error comando incorrecto")
+   elif(words[0] == "parquear" and len(words) == 2):
+      try:
+         a = int(words[1])
+         print("parqueando: " + str(a))
+      except ValueError:
+         print("Error comando incorrecto")
+   else:
+      print("Error comando incorrecto")
+   
 
-B = Button(root, text ="Send", command = send)
+test = Text(root, bg = "black")
+test.place(x=150,y=100)
+
+B = Button(root, text ="Send", command = send, bg = "green")
 B.place(x=500,y=500)
 
 E1 = Entry(root, bd =5)
