bb590d1172262f1d29ff1245f4c168b19a2f26f1
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-06-09 22:05:33 -0600

    inicio de soporte para multiples taxis

--- proyectos/Proyecto2/proyecto2.py
+++ proyectos/Proyecto2/proyecto2.py
@@ -25,6 +25,7 @@ global newClient
 global heatMap
 global X  # customizables values
 global D  # for heat map calculations
+global taxis
 
 
 delay = 0
@@ -44,6 +45,16 @@ newClient = True
 heatMap = []
 X = 2
 D = 1
+taxis = []
+
+
+#structure of node: [I,J,G,H,parentI,parentJ]
+#structure of path: [ [node1 I,node1 J],[node2 I,node2 J],...]
+#structure of block: [x,y]
+#structure of home: [pos in blocklist, people there at the moment]
+#structure of working place: [pos in blocklist, people there at the moment]
+#structure of taxi: [name,I,J,state,path,copy of map for history]
+
 
 
 #*************** GUI ***************** 
@@ -99,14 +110,14 @@ def heuristic(iniI,iniJ,endI,endJ):
                iniI-=1 #move up
             if(iniI < endI):
                iniI+=1 #move down
-            total += 15 #panalize this special case with 5 in each space
+            total += 15 #penalize this special case with 5 in each space
          if(iniI == endI):
             iniI+=2#around de corner
             if(iniJ > endJ):
                iniJ-=1 #move left
             if(iniJ < endJ):
                iniJ+=1 #move right
-            total += 15 #panalize this special case with 5 in each space
+            total += 15 #penalize this special case with 5 in each space
          
    return total
 
@@ -188,33 +199,30 @@ def Astar(iniI,iniJ,endI,endJ):
       currI = current[0]
       currJ = current[1]
       g = current[2]+1+int(heatMap[currI][currJ])
-      if(validMove(currI-1,currJ) and not inList(closedList,currI-1,currJ)): #valid to move up and not in closed list
+      
+      if(validMove(currI-1,currJ) and not inList(closedList,currI-1,currJ) and not inList(openList,currI-1,currJ)): #valid to move up and not in closed or open list
          
-         if(not inList(openList,currI-1,currJ)):
-            h = heuristic(currI-1,currJ,endI,endJ)
-            node = [currI-1,currJ,g,h,currI,currJ]
-            openList.append(node)
+         h = heuristic(currI-1,currJ,endI,endJ)
+         node = [currI-1,currJ,g,h,currI,currJ]
+         openList.append(node)
             
-      if(validMove(currI+1,currJ) and not inList(closedList,currI+1,currJ)): #valid to move down and not in closed list
+      if(validMove(currI+1,currJ) and not inList(closedList,currI+1,currJ) and not inList(openList,currI+1,currJ)): #valid to move down and not in closed or open list
          
-         if(not inList(openList,currI+1,currJ)):
-            h = heuristic(currI+1,currJ,endI,endJ)
-            node = [currI+1,currJ,g,h,currI,currJ]
-            openList.append(node)
+         h = heuristic(currI+1,currJ,endI,endJ)
+         node = [currI+1,currJ,g,h,currI,currJ]
+         openList.append(node)
 
-      if(validMove(currI,currJ-1) and not inList(closedList,currI,currJ-1)): #valid to move left and not in closed list
+      if(validMove(currI,currJ-1) and not inList(closedList,currI,currJ-1) and not inList(openList,currI,currJ-1)): #valid to move left and not in closed or open list
 
-         if(not inList(openList,currI,currJ-1)):
-            h = heuristic(currI,currJ-1,endI,endJ)
-            node = [currI,currJ-1,g,h,currI,currJ]
-            openList.append(node)
+         h = heuristic(currI,currJ-1,endI,endJ)
+         node = [currI,currJ-1,g,h,currI,currJ]
+         openList.append(node)
 
-      if(validMove(currI,currJ+1) and not inList(closedList,currI,currJ+1)): #valid to move right and not in closed list
+      if(validMove(currI,currJ+1) and not inList(closedList,currI,currJ+1) and not inList(openList,currI,currJ+1)): #valid to move right and not in closed or open list
 
-         if(not inList(openList,currI,currJ+1)):
-            h = heuristic(currI,currJ+1,endI,endJ)
-            node = [currI,currJ+1,g,h,currI,currJ]
-            openList.append(node)
+         h = heuristic(currI,currJ+1,endI,endJ)
+         node = [currI,currJ+1,g,h,currI,currJ]
+         openList.append(node)
 
    #out of while
    if(openList != []): #goal reached
@@ -258,7 +266,7 @@ def countBlocks():
                      homeBuildings.append([len(blockList)-1,0]) #structure of home: [pos in blocklist, people there at the moment]
                      
                   if(row[j] == "/"):#working place
-                     workBuildings.append([len(blockList)-1,0]) #structure of home: [pos in blocklist, people there at the moment]
+                     workBuildings.append([len(blockList)-1,0]) #structure of working place: [pos in blocklist, people there at the moment]
 
                      
                      #clients.append([len(blockList)-1,-1]) #structure of client: [begin,end] => begin at current block and end isn't generated here because not all blocks have been counted 
@@ -266,29 +274,40 @@ def countBlocks():
       i+=1
 
 
+
+#***** add taxis to line of map ******
+
+def taxisInLine(i):
+   global taxis
+   global mapa
+   line = mapa[i]
+   j=0
+   while(j<len(taxis)):
+      if(taxis[j][1] == i): #the taxi is in that line
+         line = line[0:taxis[j][2]] + taxis[j][0] + line[taxis[j][2]+1:]
+      j+=1
+   return line
+         
+
+
 #**** Map to string for printing *****
 
 def mapToPlain():
-   global mapa
    global mostrar
    global ruta
    s = ""
    i = 0                 
-   while(i<len(mapa)-1):
-      line = mapa[i]
-      if(mostrar == False):
-         line = line.replace("*"," ")
-      if(ruta == False):
-         line = line.replace("+"," ")
-      s = s + line + "\n"
+   while(i<len(mapa)):
+      line = taxisInLine(i)
+      s += line + "\n"
       i = i+1
-   return s[:-1] #eliminate last "\n"
+   return s 
 
 
 
 #********** begin heat map ***********
 
-def beginHeat():
+def beginHeatMap():
    global heatMap
    global mapa
 
@@ -314,30 +333,97 @@ def readBoard():
    file = open("board.txt", "r")
    plainBoard = file.read()
    mapa = plainBoard.split("\n")
+   mapa = mapa[0:len(mapa)-1] #eliminate final \n
    countBlocks()
-   beginHeat()
+   beginHeatMap()
+
+
+
+#*************************************
+
+def hireTaxis(n):
+   global taxis
+   global mapa
+   names = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+   if(n>=len(names)):
+      n=len(names)-1 #fix max amount of taxis
+   i=0
+   while(i<n):
+      posI = random.randint(0,len(mapa)-1)
+      posJ = random.randint(0,len(mapa[posI])-1)
+      while(not validMove(posI,posJ)):
+         posI = random.randint(0,len(mapa)-1)
+         posJ = random.randint(0,len(mapa[posI])-1)
+            
+      state = 0
+      path=[]
+      history = mapa
+      taxis.append([names[i],posI,posJ,state,path,history]) #taxi structure: [name,I,J,state,path,copy of map for history]
+      i+=1
    
 
 
 #********** parse command ************
    
 def send():
-   readBoard()
-   rePaint()
-   print(Astar(1,2,5,11))
+
+   global blockList
+   global delay
+   global mostrar
+   global ruta
+   global clients
+   global pasear
+   global parquear
+   global buscar
+   global spot
    
+   words = getAction()
+
+   if(words[0] == "animar" and len(words)==2):
+      n = words[1]
+      
+   elif(words[0] == "clientes" and len(words)==2):
+      n = words[1]
+      
+   elif(words[0] == "cliente" and len(words)==3):
+      home = words[1]
+      work = words[2]
+      
+   elif(words[0] == "taxis" and len(words)==2):
+      print(words)
+      n = int(words[1])
+      print(n)
+      hireTaxis(n)
+      rePaint()
+      
+   elif(len(words[0])==1): #actions for one taxi
+      taxi = words[0]
+      
+   else:
+      print("error, wrong action")
+   
+     
+   
+#********* get text in entry *********
+
+def getAction():
+   action = E1.get()
+   E1.delete(0,END)
+   words = action.split(" ")
+   return words
 
 
 #********** refresh GUI **************
    
-def rePaint():
+def rePaint(): 
    global test
-   
+
    test.destroy()
 
    test = Text(root, bg = "black",fg = "green")
    test.place(x=150,y=100)
    test.insert(INSERT,mapToPlain())
+
 
 
 
@@ -352,5 +438,11 @@ B.place(x=500,y=500)
 
 E1 = Entry(root, bd =5)
 E1.place(x=300,y=500)
+
+
+readBoard()
+rePaint()
+#print(Astar(1,2,5,11))
+
 
 root.mainloop()
