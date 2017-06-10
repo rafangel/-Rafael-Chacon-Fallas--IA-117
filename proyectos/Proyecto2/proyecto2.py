a6cc2d0edeb70e41a532c5d54faa769ec55bd21c
Rafael Chacón Fallas <rafangel1228@gmail.com>
2017-06-09 19:50:33 -0600

    incorporado el congestionamiento al calculo del A star y cambios menores al mapa

--- proyectos/Proyecto2/proyecto2.py
+++ proyectos/Proyecto2/proyecto2.py
@@ -14,26 +14,36 @@ global pasear
 global parquear
 global buscar
 global blockList
+global homeBuildings
+global workBuildings
 global test
 global clients
 global lastPos
 global spot
 global free
 global newClient
+global heatMap
+global X  # customizables values
+global D  # for heat map calculations
 
 
 delay = 0
-blockList = []
-clients = []
 mostrar = False
 ruta = False
 pasear = False
 parquear = False
 buscar = False
+blockList = []
+homeBuildings = []
+workBuildings = []
+clients = []
 lastPos = []
 spot = []
 free = True
 newClient = True
+heatMap = []
+X = 2
+D = 1
 
 
 #*************** GUI ***************** 
@@ -45,28 +55,58 @@ root.configure(background='black')
 
 
 
-#**estimated distance ignoring walls**
+
+#****** check if pos is valid ********
+
+def validMove(I,J):
+   global mapa
+   if(I >= 0 and I < len(mapa)):
+      if(J >= 0 and J < len(mapa[I])):
+         if(mapa[I][J] == " " or mapa[I][J] == "+" or mapa[I][J] == "*"):
+            return True
+         
+   return False
+
+
+
+#******* estimated distance *********
 
 def heuristic(iniI,iniJ,endI,endJ):
-   
+   global heatMap
    total = 0
-   while(iniI != endI and iniJ != endJ):
+   while(iniI != endI or iniJ != endJ):
       
-      if(iniI > endI):
+      if(iniI > endI and validMove(iniI-1,iniJ)):
          iniI-=1 #move up
-         total +=1
+         total += 1 + int(heatMap[iniI][iniJ])
          
-      if(iniI < endI):
+      elif(iniI < endI and validMove(iniI+1,iniJ)):
          iniI+=1 #move down
-         total +=1
+         total += 1 + int(heatMap[iniI][iniJ])
 
-      if(iniJ > endJ):
+      elif(iniJ > endJ and validMove(iniI,iniJ-1)):
          iniJ-=1 #move left
-         total +=1
+         total += 1 + int(heatMap[iniI][iniJ])
          
-      if(iniJ < endJ):
+      elif(iniJ < endJ and validMove(iniI,iniJ+1)):
          iniJ+=1 #move right
-         total +=1
+         total += 1 + int(heatMap[iniI][iniJ])
+         
+      else:#rare cases
+         if(iniJ == endJ):
+            iniJ+=2#around de corner
+            if(iniI > endI):
+               iniI-=1 #move up
+            if(iniI < endI):
+               iniI+=1 #move down
+            total += 15 #panalize this special case with 5 in each space
+         if(iniI == endI):
+            iniI+=2#around de corner
+            if(iniJ > endJ):
+               iniJ-=1 #move left
+            if(iniJ < endJ):
+               iniJ+=1 #move right
+            total += 15 #panalize this special case with 5 in each space
          
    return total
 
@@ -101,17 +141,6 @@ def lowestF(openList):
    return pos
 
 
-#****** check if pos is valid ********
-
-def validMove(I,J):
-   global mapa
-   if(I >= 0 and I < len(mapa)):
-      if(J >= 0 and J < len(mapa[I])):
-         if(mapa[I][J] == " " or mapa[I][J] == "+" or mapa[I][J] == "*"):
-            return True
-         
-   return False
-
 
 #**** checks if node is in list ****
 
@@ -140,49 +169,50 @@ def getNode(lista,I,J):
 
 def Astar(iniI,iniJ,endI,endJ):
    global mapa
+   global heatMap
    openList = []
    closedList = []
-
+   
    h=heuristic(iniI,iniJ,endI,endJ)
    current = [iniI,iniJ,0,h,iniI,iniJ] #structure of node: [I,J,G,H,parentI,parentJ]
    
    
    openList.append(current)
 
-   while(openList != [] and goalNotFound(endI,endJ,closedList)):
 
+   while(openList != [] and goalNotFound(endI,endJ,closedList)):
+      
       pos = lowestF(openList)
       current = openList.pop(pos)
       closedList.append(current)
       currI = current[0]
       currJ = current[1]
-      g = current[2]+1
-      
+      g = current[2]+1+int(heatMap[currI][currJ])
       if(validMove(currI-1,currJ) and not inList(closedList,currI-1,currJ)): #valid to move up and not in closed list
          
          if(not inList(openList,currI-1,currJ)):
-            h = heuristic(iniI,iniJ,currI-1,currJ)
+            h = heuristic(currI-1,currJ,endI,endJ)
             node = [currI-1,currJ,g,h,currI,currJ]
             openList.append(node)
             
       if(validMove(currI+1,currJ) and not inList(closedList,currI+1,currJ)): #valid to move down and not in closed list
          
          if(not inList(openList,currI+1,currJ)):
-            h = heuristic(iniI,iniJ,currI+1,currJ)
+            h = heuristic(currI+1,currJ,endI,endJ)
             node = [currI+1,currJ,g,h,currI,currJ]
             openList.append(node)
 
       if(validMove(currI,currJ-1) and not inList(closedList,currI,currJ-1)): #valid to move left and not in closed list
 
          if(not inList(openList,currI,currJ-1)):
-            h = heuristic(iniI,iniJ,currI,currJ-1)
+            h = heuristic(currI,currJ-1,endI,endJ)
             node = [currI,currJ-1,g,h,currI,currJ]
             openList.append(node)
 
       if(validMove(currI,currJ+1) and not inList(closedList,currI,currJ+1)): #valid to move right and not in closed list
 
          if(not inList(openList,currI,currJ+1)):
-            h = heuristic(iniI,iniJ,currI,currJ+1)
+            h = heuristic(currI,currJ+1,endI,endJ)
             node = [currI,currJ+1,g,h,currI,currJ]
             openList.append(node)
 
@@ -206,6 +236,8 @@ def Astar(iniI,iniJ,endI,endJ):
 
 def countBlocks():
    global blockList
+   global homeBuildings
+   global workBuildings
    global mapa
    global clients
    blockList = [] #reset variable
@@ -215,17 +247,23 @@ def countBlocks():
       j = 1                      #ignore first
       while(j<len(row)-2):       #and last column
          #print("in loop: " + row + " with i: "+ str(i) + " j: " + str(j) + " value: " + row[j])
-         #print()
          if(row[j-1]=="|" and row[j+1]=="|"):
             prevRow =mapa[i-1]
             nextRow = mapa[i+1]
-            if(len(prevRow)>j and len(nextRow)>j): #map shapes
+            if(len(prevRow)>j and len(nextRow)>j): #checks maps dimensions 
                if(prevRow[j]=="-" and nextRow[j]=="-"):#block detected
                   blockList.append([j,i]) #structure of block: [x,y]
-                  if(row[j] != "."): #client on block
-                     clients.append([len(blockList)-1,-1]) #structure of client: [begin,end] => begin at current block and end isn't generated here because not all blocks have been counted 
-         j= j+1
-      i= i+1
+
+                  if(row[j] == "."): #homes
+                     homeBuildings.append([len(blockList)-1,0]) #structure of home: [pos in blocklist, people there at the moment]
+                     
+                  if(row[j] == "/"):#working place
+                     workBuildings.append([len(blockList)-1,0]) #structure of home: [pos in blocklist, people there at the moment]
+
+                     
+                     #clients.append([len(blockList)-1,-1]) #structure of client: [begin,end] => begin at current block and end isn't generated here because not all blocks have been counted 
+         j+=1
+      i+=1
 
 
 #**** Map to string for printing *****
@@ -248,6 +286,25 @@ def mapToPlain():
 
 
 
+#********** begin heat map ***********
+
+def beginHeat():
+   global heatMap
+   global mapa
+
+   i=0
+   while(i<len(mapa)-1):
+      j=0
+      heatMap.append(mapa[i])
+      while(j<len(mapa[i])-1):
+         if(mapa[i][j] == " "):
+            heatMap[i] = heatMap[i][0:j] +"0"+ heatMap[i][j+1:]#initial
+         j+=1
+      
+      i+=1
+   
+
+
 #********** Read File ****************
 
 def readBoard():
@@ -257,8 +314,9 @@ def readBoard():
    file = open("board.txt", "r")
    plainBoard = file.read()
    mapa = plainBoard.split("\n")
-
    countBlocks()
+   beginHeat()
+   
 
 
 #********** parse command ************
